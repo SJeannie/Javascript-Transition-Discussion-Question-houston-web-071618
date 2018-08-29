fa37ecdaacaebf0a9cbd1a0e18194eb4da6b2d65
Jeannie Kim <SJ.Jeannie.Kim@gmail.com>
2018-08-29 11:01:15 -0500

    Bring Lincoln back

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(function() {
+//   let duration = $('#duration').val();
+//   saveLincoln(parseInt(duration));
+// });
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+let fadeTarget = document.getElementById('foreground');
+
+let duration = document.getElementById('duration');
+
+document.getElementById('save_lincoln').addEventListener('click', function() {
+  saveLincoln(parseInt(duration.value));
+});
+
+document.getElementById('kill_lincoln').addEventListener('click', function() {
+  fadeTarget.style.opacity = 1;
+});
+
+function saveLincoln(duration) {
+  if (isNaN(duration)) {
+    duration = 5000;
   }
-  $('#foreground').fadeOut(interval);
+  console.log(duration); // This is when duration
+
+  fadeOutEffect();
+  // $('#foreground').fadeOut(interval);
+
+  function fadeOutEffect() {
+    console.log('Work!');
+    let interval = duration / 10;
+    let fadeEffect = setInterval(function() {
+      if (!fadeTarget.style.opacity) {
+        fadeTarget.style.opacity = 1;
+      }
+      if (fadeTarget.style.opacity > 0) {
+        fadeTarget.style.opacity -= 0.1;
+        console.log(
+          `Oh yay, I'm coming back... x${fadeTarget.style.opacity * 10}`
+        );
+      } else {
+        clearInterval(fadeEffect);
+      }
+    }, interval);
+  }
 }
